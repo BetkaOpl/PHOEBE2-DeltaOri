529d53d4627f44bcc6bfdfe89c687d169fd0c159
BetkaOpl <55211191+BetkaOpl@users.noreply.github.com>
2022-04-20 14:16:11 +0200

    Add files via upload

---
+++ test_omp.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from multiprocessing import Pool
+
+from myphoebe import *
+
+def main():
+  '''
+  Test OpenMP parallelisation (1 node, 1 cpu, >threads).
+  '''
+
+  myphoebe = Myphoebe()
+
+  pool = Pool()
+  run_mcmc(myphoebe, pool=pool)
+
+if __name__ == "__main__":
+  main()
